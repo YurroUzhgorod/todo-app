94a6780d07357fa3ae188a058586d680c566286e
Yuriy Kuliaba <yuriykuliaba@gmail.com>
2023-02-25 14:24:11 +0200

    connected styles to todo-form component and added additional settings

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./TodoForm.module.css";
 
 function TodoForm({ addTodo }) {
   const [text, setText] = useState("");
@@ -14,16 +15,18 @@ function TodoForm({ addTodo }) {
     setText(value);
   };
   return (
-    <form onSubmit={onSubmitHandler}>
-      <input
-        placeholder="Enter new todo"
-        value={text}
-        onChange={onChangeHandler}
-      />
-      <button type="submit" onClick={onSubmitHandler}>
-        Submit
-      </button>
-    </form>
+    <div className={styles.todoFormContainer}>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          placeholder="Enter new todo"
+          value={text}
+          onChange={onChangeHandler}
+        />
+        <button type="submit" onClick={onSubmitHandler}>
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 
